c3dfe3276d961a6e06581682b98eda444ccdaa57
Milot Mirdita <milot@mirdita.de>
2023-03-26 01:05:48 +0900

    Fixed signed/unsigned confusion for compressed ca

--- src/server/compressed_ca.mjs
+++ src/server/compressed_ca.mjs
@@ -12,36 +12,36 @@ export default function(mem, chainLength, entryLength) {
 
     let offset = 0;
     let diffSum = 0;
-    let start = mem.readUInt32LE(offset);
+    let start = mem.readInt32LE(offset);
     offset += intSize;
     buffer[0] = start / 1000.0;
 
     for (let i = 1; i < chainLength; ++i) {
-        const intDiff = mem.readUInt16LE(offset);
+        const intDiff = mem.readInt16LE(offset);
         offset += shortSize;
         diffSum += intDiff;
         buffer[i] = (start + diffSum) / 1000.0;
     }
 
     diffSum = 0;
-    start = mem.readUInt32LE(offset);
+    start = mem.readInt32LE(offset);
     offset += intSize;
     buffer[chainLength] = start / 1000.0;
 
     for (let i = chainLength + 1; i < 2 * chainLength; ++i) {
-        const intDiff = mem.readUInt16LE(offset);
+        const intDiff = mem.readInt16LE(offset);
         offset += shortSize;
         diffSum += intDiff;
         buffer[i] = (start + diffSum) / 1000.0;
     }
 
     diffSum = 0;
-    start = mem.readUInt32LE(offset);
+    start = mem.readInt32LE(offset);
     offset += intSize;
     buffer[2 * chainLength] = start / 1000.0;
 
     for (let i = 2 * chainLength + 1; i < 3 * chainLength; ++i) {
-        const intDiff = mem.readUInt16LE(offset);
+        const intDiff = mem.readInt16LE(offset);
         offset += shortSize;
         diffSum += intDiff;
         buffer[i] = (start + diffSum) / 1000.0;
