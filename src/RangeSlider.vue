f18c69e993533e36419f4d0743b644deac6d9cc1
yeojingi <businessjingi@gmail.com>
2023-07-13 20:52:05 +0900

    frontend

---
+++ src/RangeSlider.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-card style="padding: 2em; width: 250px;">
+    <h3>Filter by</h3>
+    <v-text-field
+        :model-value="range[0]"
+        :value="range[0]"
+        @change="$set(range, 0, $event)"
+        label="min"
+        single-line
+        type="number"
+        variant="outlined"
+        density="compact"
+        style="width: 80px; display: inline-block;">
+    </v-text-field>
+    ~
+    <v-text-field
+        :model-value="range[1]"
+        :value="range[1]"
+        label="min"
+        single-line
+        type="number"
+        variant="outlined"
+        density="compact"
+        style="width: 80px; display: inline-block;"
+        @change="$set(range, 1, $event)">
+    </v-text-field>
+</v-card>
+</template>
+
+<script>
+
+export default {
+  name: "RangeSlider",
+  props: ["range"],
+  computed: {
+  }
+}
+
+</script>
--- src/Search.vue
+++ src/Search.vue
@@ -34,6 +34,7 @@
                                 dark
                             >
                                 <v-tab>UniProt</v-tab>
+                                <v-tab>Gene ontology</v-tab>
                                 <v-tab>Structure</v-tab>
                             </v-tabs>
                             <v-tabs-items v-model="tab" style="padding: 1em;">
@@ -56,22 +57,50 @@
                                     </v-text-field>
                                     
                                     <template>
-                                    <h2 class="text-h6 mb-2">
-                                        Examples
-                                    </h2>
-                                    <v-chip-group
-                                        column
-                                        dark
-                                        v-model="selectedExample"
-                                        style="max-width: 400px; margin: 0 auto; "
-                                    >
+                                        <h2 class="text-h6 mb-2">
+                                            Examples
+                                        </h2>
+                                        <v-chip-group
+                                            column
+                                            dark
+                                            v-model="selectedExample"
+                                            style="max-width: 400px; margin: 0 auto; "
+                                        >
 
-                                        <v-chip v-for="item in examples" :key="item.id"
-                                            outlined v-on:click="query=item.id" >
-                                            <b>{{ item.id }}</b> &emsp; {{ item.desc }}
-                                        </v-chip>
-                                    </v-chip-group>
-                                </template>
+                                            <v-chip v-for="item in examples" :key="item.id"
+                                                outlined v-on:click="query=item.id" >
+                                                <b>{{ item.id }}</b> &emsp; {{ item.desc }}
+                                            </v-chip>
+                                        </v-chip-group>
+                                    </template>
+                                </v-tab-item>
+                                <v-tab-item>
+                                    <v-text-field
+                                        outlined
+                                        label="GO ID"
+                                        style="max-width: 400px; margin: 0 auto;"
+                                        v-model="query_GO"
+                                        :append-icon="inSearch ? $MDI.mdiProgressWrench : $MDI.Magnify"
+                                        :disabled="inSearch"
+                                        @click:append="GOsearch"
+                                        @keyup.enter="GOsearch"
+                                        @change="selectedExample = null"
+                                        @keydown="error = null"
+                                        :error="error != null"
+                                        :error-messages="error ? error : []"
+                                        dark
+                                        >
+                                    </v-text-field>
+                                    <v-radio-group 
+                                        style="
+                                            max-width: 400px;
+                                            margin: 0 auto;
+                                            "
+                                        v-model="go_search_type"
+                                        inline>
+                                        <v-radio name="go_search_type" label="include lower GO lineage" value="lower" dark></v-radio>
+                                        <v-radio name="go_search_type" label="Exact" value="exact" dark ></v-radio>
+                                    </v-radio-group>
                                 </v-tab-item>
                                 <v-tab-item>
                                     <FoldseekSearchButton @response="foldseekResult" dark></FoldseekSearchButton>
@@ -88,17 +117,16 @@
                     </template>
                     <template slot="toolbar-extra">
                     </template>
+                    <GoTermResultList goterm=""></GoTermResultList>
                     <template slot="content">
                         <v-data-table
                             :headers="headers"
                             :items="response"
+                            :options.sync="options"
+                            :server-items-length="options.totalMembers"
                         >
                             <template v-slot:item.rep_accession="prop">
                                 <router-link :to="{ name: 'cluster', params: { cluster: prop.value }}" target='_blank'>{{ prop.value }}</router-link>
-                            </template>
-
-                            <template v-slot:item.avg_len="prop">
-                                {{ prop.value.toFixed(2) }}
                             </template>
 
                             <template v-slot:item.avg_plddt="prop">
@@ -110,10 +138,109 @@
                                 {{ prop.value.toFixed(2) }}
                             </template>
 
+                            <template v-slot:header.lca_tax_id="{ header }">
+                                    <TaxonomyAutocomplete
+                                        cluster="null"
+                                        v-model="options.tax_id"
+                                        :urlFunction="(a, b) => '/cluster/' + a + '/search/taxonomy/' + b"
+                                        :disabled="taxAutocompleteDisabled"
+                                        :bundle_data="bundle_original"
+                                    ></TaxonomyAutocomplete>
+                            </template>
 
                             <template v-slot:item.lca_tax_id="prop">
                                 <TaxSpan :taxonomy="prop.value"></TaxSpan>
                             </template>
+
+                            <template v-slot:header.is_dark="{ header }">
+                                <v-menu
+                                    :close-on-content-click="false"
+                                    offset-y>
+                                    <template v-slot:activator="{ on }">
+                                        <v-btn v-on="on" :outlined="options.is_dark != null">
+                                            {{ header.text }}
+                                        </v-btn>
+                                    </template>
+
+                                    <v-card style="padding: 2em; width: 250px;">
+                                        <h3>Filter by</h3>
+                                        <v-chip-group column v-model="options.is_dark">
+                                            <IsDark isDark="0"></IsDark>
+                                            <IsDark isDark="1"></IsDark>
+                                        </v-chip-group>
+                                    </v-card>
+                                </v-menu>
+                            </template>
+
+                            <template v-slot:item.is_dark="prop">
+                                <IsDark :isDark="prop.value"></IsDark>
+                            </template>
+
+                            <template v-slot:header.avg_len="{ header }">
+                                <v-menu
+                                    :close-on-content-click="false"
+                                    offset-y>
+                                    <template v-slot:activator="{ on }">
+                                        <v-btn v-on="on" >
+                                            {{ header.text }}
+                                        </v-btn>
+                                    </template>
+                                    <RangeSlider :range="options.avg_length_range"></RangeSlider>
+                                </v-menu>
+                            </template>
+
+                            <template v-slot:header.avg_plddt="{ header }">
+                                <v-menu
+                                    :close-on-content-click="false"
+                                    offset-y>
+                                    <template v-slot:activator="{ on }">
+                                        <v-btn v-on="on" >
+                                            {{ header.text }}
+                                        </v-btn>
+                                    </template>
+                                    <RangeSlider :range="options.avg_plddt_range"></RangeSlider>
+                                </v-menu>
+                            </template>
+
+                            <template v-slot:header.n_mem="{ header }">
+                                <v-menu
+                                    :close-on-content-click="false"
+                                    offset-y>
+                                    <template v-slot:activator="{ on }">
+                                        <v-btn v-on="on" >
+                                            {{ header.text }}
+                                        </v-btn>
+                                    </template>
+                                    <RangeSlider :range="options.n_mem_range"></RangeSlider>
+                                </v-menu>
+                            </template>
+
+                            <template v-slot:header.rep_len="{ header }">
+                                <v-menu
+                                    :close-on-content-click="false"
+                                    offset-y>
+                                    <template v-slot:activator="{ on }">
+                                        <v-btn v-on="on" >
+                                            {{ header.text }}
+                                        </v-btn>
+                                    </template>
+                                    <RangeSlider :range="options.rep_length_range"></RangeSlider>
+                                </v-menu>
+                            </template>
+
+                            <template v-slot:header.rep_plddt="{ header }">
+                                <v-menu
+                                    :close-on-content-click="false"
+                                    offset-y>
+                                    <template v-slot:activator="{ on }">
+                                        <v-btn v-on="on" >
+                                            {{ header.text }}
+                                        </v-btn>
+                                    </template>
+                                    <RangeSlider :range="options.rep_plddt_range"></RangeSlider>
+                                </v-menu>
+                            </template>
+
                         </v-data-table>
                     </template>
                 </panel>
@@ -141,6 +268,9 @@
 import Panel from "./Panel.vue";
 import TaxSpan from "./TaxSpan.vue";
 import FoldseekSearchButton from "./FoldseekSearchButton.vue";
+import TaxonomyAutocomplete from "./TaxonomyAutocomplete.vue";
+import IsDark from './IsDark.vue';
+import RangeSlider from './RangeSlider.vue';
 
 export default {
     name: "search",
@@ -148,10 +278,14 @@ export default {
         Panel,
         FoldseekSearchButton,
         TaxSpan,
+        TaxonomyAutocomplete,
+        IsDark,
+        RangeSlider
     },
     data() {
         return {
             query: "B4DKH6",
+            query_GO: "GO:0005524",
             selectedExample: 1,
             tab: 0,
             examples: [
@@ -162,11 +296,28 @@ export default {
             ],
             inSearch: false,
             response: null,
+            bundle_original: null,
+            total: null,
+            page: null,
             error: null,
             headers: [
                 {
                     text: "Rep Accession",
                     value: "rep_accession",
+                },
+                {
+                    text: "LCA rank",
+                    value: "lca_tax_id.rank"
+                },
+                // {
+                //     text: "LCA",
+                //     value: "lca_tax_id.name",
+                //     sortable: false,
+                // },
+                {
+                    text: "LCA",
+                    value: "lca_tax_id",
+                    sortable: false,
                 },
                 {
                     text: "Average Length",
@@ -180,13 +331,10 @@ export default {
                     text: "Number of members",
                     value: "n_mem",
                 },
-                // {
-                //     text: "LCA Taxonomy",
-                //     value: "lca_tax_id",
-                // },
                 {
-                    text: "Is Dark",
+                    text: "Dark",
                     value: "is_dark",
+                    sortable: false,
                 },
                 {
                     text: "Rep pLDDT",
@@ -196,8 +344,31 @@ export default {
                     text: "Rep Length",
                     value: "rep_len",
                 },
-            ]
+            ],
+            go_search_type: "lower",
+            options: {
+                search_type: null,
+                avg_length_range: [Infinity, 0],
+                avg_plddt_range: [Infinity, 0],
+                rep_length_range: [Infinity, 0],
+                rep_plddt_range: [Infinity, 0],
+                n_mem_range: [Infinity, 0],
+                showDarkOnly: false,
+                tax_id: null,
+                is_dark: null,
+                totalMembers: null,
+            },
+            taxAutocompleteDisabled: false,
+            range: [5, 5],
         };
+    },
+    watch : {
+        options: {
+            handler () {
+                this.filterData()
+            },
+            deep: true,
+        }
     },
     computed: {
         windowHeight() {
@@ -229,8 +400,28 @@ export default {
                     this.inSearch = false;
                 });
         },
+        GOsearch() {
+            this.inSearch = true;
+            this.error = null;
+            this.$axios.post("/go/" + this.query_GO, 
+                {
+                    itemsPerPage: 15, page: 1,
+                    go_search_type: this.go_search_type
+                })
+                .then(res => {
+                    this.response = res.data.result;
+                    console.log(this.response)
+                    this.options.search_type = "go";
+                    this.bundle_original = res.data.result;
+                    this.init_range();
+                }
+                );
+        },
         foldseekResult(result) {
             this.response = result;
+            this.options.search_type = "foldseek";
+            this.bundle_original = result;
+            this.init_range();
             this.$nextTick(() => {
                 this.$refs.results.scrollIntoView({
                     behavior: "smooth",
@@ -238,6 +429,65 @@ export default {
                     inline: "nearest"
                 })
             })
+        },
+        filterData () {
+            this.loading = true;
+            const options = {};
+
+            if (this.options.search_type === 'go') {
+                options['query_GO'] = this.query_GO;
+                options["go_search_type"] = this.go_search_type;
+                options["bundle"] = this.bundle_original;
+            }
+            else if (this.options.search_type === 'foldseek') {
+                options["bundle"] = this.bundle_original;
+            }
+
+            let request = Object.assign({}, this.options, options);
+
+            this.$axios.post("/search/filter" , request)
+                .then(response => {
+                    this.response = response.data.result;
+                    this.options.totalMembers = response.data.total;
+                })
+                .catch(() => {})
+                .finally(() => {
+                    this.loading = false;
+                });
+        },
+        init_range() {
+            this.bundle_original.forEach(x => {
+                if (x.avg_len < this.options.avg_length_range[0]) {
+                    this.options.avg_length_range[0] = x.avg_len;
+                }
+                if (x.avg_len > this.options.avg_length_range[1]) {
+                    this.options.avg_length_range[1] = x.avg_len;
+                }
+                if (x.rep_len < this.options.rep_length_range[0]) {
+                    this.options.rep_length_range[0] = x.rep_len;
+                }
+                if (x.rep_len > this.options.rep_length_range[1]) {
+                    this.options.rep_length_range[1] = x.rep_len;
+                }
+                if (x.avg_plddt < this.options.avg_plddt_range[0]) {
+                    this.options.avg_plddt_range[0] = x.avg_plddt;
+                }
+                if (x.avg_plddt > this.options.avg_plddt_range[1]) {
+                    this.options.avg_plddt_range[1] = x.avg_plddt;
+                }
+                if (x.rep_plddt < this.options.rep_plddt_range[0]) {
+                    this.options.rep_plddt_range[0] = x.rep_plddt;
+                }
+                if (x.rep_plddt > this.options.rep_plddt_range[1]) {
+                    this.options.rep_plddt_range[1] = x.rep_plddt;
+                }
+                if (x.n_mem < this.options.n_mem_range[0]) {
+                    this.options.n_mem_range[0] = x.n_mem;
+                }
+                if (x.n_mem > this.options.n_mem_range[1]) {
+                    this.options.n_mem_range[1] = x.n_mem;
+                }
+            });
         }
     }
 };
