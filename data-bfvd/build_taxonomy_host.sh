bc5f66ef84a8fc1677a5af4db0a8e88e84a99ce8
Milot Mirdita <milot@mirdita.de>
2024-10-19 19:06:50 +0900

    Add script to load hosts

---
+++ data-bfvd/build_taxonomy_host.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# ./build_taxonomy_host.sh afdb-clusters.sqlite3 ./taxonomy-accession_host.tsv
+
+sqlite3 $1 << EOF
+
+CREATE TABLE taxonomy_host (
+  accession TEXT PRIMARY KEY,
+  tax_id TEXT,
+);
+
+.mode tabs
+
+.import "${2}" taxonomy_host
+
+CREATE INDEX taxonomy_host_idx
+ON taxonomy_host(accession);
